ec4411f146f0811bebb2ae3d0dd229a77072ed43
breno.breves <brenocsb@dcc.ufrj.br>
2023-11-26 17:05:32 -0300

    feat: Right pannel welcome message

--- src/components/RightPannel.jsx
+++ src/components/RightPannel.jsx
@@ -5,10 +5,45 @@ function RightPannel({weather, deg, forecast}) {
 
   return ( 
     <SCRightPannel>
-
+        {!weather || !forecast ?
+        <>
+            <SCWellcomeMsg>
+                Bem vindo!
+            </SCWellcomeMsg>
+            <SCWellcomeDesc>
+                Para onde vamos?
+            </SCWellcomeDesc>
+        </>
+        :"Com dados"}
     </SCRightPannel>
   )
 }
+
+const SCWellcomeDesc = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items:flex-start;
+    padding:10px 0px 0px 110px;
+    color: #222;
+    font-family: Poppins, sans-serif;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 60px; 
+`
+
+const SCWellcomeMsg = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items:flex-start;
+    padding:50px 0px 0px 100px;
+    color: #222;
+    font-family: Poppins, sans-serif;
+    font-size: 150px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160px; 
+`
 
 const SCRightPannel = styled.div`
   height:100%;
