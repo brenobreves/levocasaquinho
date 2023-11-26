bd172a9b1b3b1434871160a6b220b13a947f4d80
breno.breves <brenocsb@dcc.ufrj.br>
2023-11-26 17:12:02 -0300

    refactor: wellcome msg now as a component

--- src/components/RightPannel.jsx
+++ src/components/RightPannel.jsx
@@ -1,55 +1,27 @@
 import { useState } from 'react'
 import styled from 'styled-components'
+import WellcomeMsg from './WellcomeMsg'
 
 function RightPannel({weather, deg, forecast}) {
-
+    const [today, setToday] = useState(true)
   return ( 
     <SCRightPannel>
         {!weather || !forecast ?
+        <WellcomeMsg/>
+        :
         <>
-            <SCWellcomeMsg>
-                Bem vindo!
-            </SCWellcomeMsg>
-            <SCWellcomeDesc>
-                Para onde vamos?
-            </SCWellcomeDesc>
+            
         </>
-        :"Com dados"}
+        }
     </SCRightPannel>
   )
 }
 
-const SCWellcomeDesc = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items:flex-start;
-    padding:10px 0px 0px 110px;
-    color: #222;
-    font-family: Poppins, sans-serif;
-    font-size: 30px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 60px; 
-`
 
-const SCWellcomeMsg = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items:flex-start;
-    padding:50px 0px 0px 100px;
-    color: #222;
-    font-family: Poppins, sans-serif;
-    font-size: 150px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 160px; 
-`
 
 const SCRightPannel = styled.div`
   height:100%;
   width: 65%;
   background-color:#EFEFEF;
 `
-
-
 export default RightPannel
---
+++ src/components/WellcomeMsg.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+function WellcomeMsg() {
+  return ( 
+        <>
+            <SCWellcomeMsg>
+                Bem vindo!
+            </SCWellcomeMsg>
+            <SCWellcomeDesc>
+                Para onde vamos?
+            </SCWellcomeDesc>
+        </>
+  )
+}
+
+const SCWellcomeDesc = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items:flex-start;
+    padding:10px 0px 0px 110px;
+    color: #222;
+    font-family: Poppins, sans-serif;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 60px; 
+`
+
+const SCWellcomeMsg = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items:flex-start;
+    padding:50px 0px 0px 100px;
+    color: #222;
+    font-family: Poppins, sans-serif;
+    font-size: 150px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160px; 
+`
+
+const SCRightPannel = styled.div`
+  height:100%;
+  width: 65%;
+  background-color:#EFEFEF;
+`
+
+
+export default WellcomeMsg
